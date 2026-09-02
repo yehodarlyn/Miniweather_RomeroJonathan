4f04b7fd4adf225e1721f3994905a9bf357d4408
Jonathan Romero <109704905+yehodarlyn@users.noreply.github.com>
2026-09-02 14:22:02 -0700

    Practica 1

---
+++ Miniweather_RomeroJ/app/src/main/java/com/example/miniweather_romeroj/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.miniweather_romeroj
+
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        var miTexto = findViewById<TextView>(R.id.tvGreeting)
+        var miTexto2 = findViewById<TextView>(R.id.tvCity)
+        var miTexto3 = findViewById<TextView>(R.id.ivTemperature)
+        var miTexto4 = findViewById<TextView>(R.id.tvWeather)
+
+        var miImagen = findViewById<ImageView>(R.id.ivWeather)
+    }
+}
